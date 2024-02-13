8905cbd7ef6eaa08e62fc14576d194ea863b20ba
saurabhthakre1721999 <saurabhthakre1721999@gmail.com>
2024-02-13 16:22:07 +0530

    fix css

--- vite-project/src/components/Atoms/header.jsx
+++ vite-project/src/components/Atoms/header.jsx
@@ -1,12 +1,32 @@
 import React from "react";
+import { Box, Grid } from "@mui/material";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 const Header = () => {
   return (
-    <>
-      <header>
-        {" "}
-        <div>Home</div> <div>About</div> <div>Contact</div>
-      </header>
-    </>
+    <Box>
+      <Grid container spacing={1} justifyContent="flex-end">
+        <Grid item xs={1}>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <Style>Home</Style>
+          </Link>
+        </Grid>
+        <Grid item xs={1}>
+          <Link to="/about" style={{ textDecoration: "none" }}>
+            <Style>About</Style>
+          </Link>
+        </Grid>
+        <Grid item xs={1}>
+          <Link to="/contact" style={{ textDecoration: "none" }}>
+            <Style>Contact</Style>
+          </Link>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 export default Header;
+const Style = styled.span`
+  text-decoration: none;
+  color: black;
+`;
